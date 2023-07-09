26e5431b9b1bf25402b44bee4e2324f99ab49a57
Sergey Rubnikovich <sergeyrubnikovich@yandex.ru>
2023-07-09 14:04:51 +0700

    Make migrations on startup

--- webtronics/main.py
+++ webtronics/main.py
@@ -1,4 +1,5 @@
 import fastapi as fa
+from alembic import command
 from api import api_router
 from api.exc import authjwt_exception_handler, validation_exception_handler
 from enums import Stages
@@ -6,10 +7,16 @@ from fastapi.exceptions import RequestValidationError
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from fastapi_pagination import add_pagination
 from settings import SETTINGS
+from tests.utils import alembic_config_from_url
 
 
 SWAGGER_PATH = "/swagger"
 OPENAPI_PATH = "/openapi"
+
+
+def make_migrations():
+    alembic_config = alembic_config_from_url(SETTINGS.get_sync_database_uri())
+    command.upgrade(alembic_config, "head")
 
 
 def get_app() -> fa.FastAPI:
@@ -31,6 +38,7 @@ def get_app() -> fa.FastAPI:
 
     # добавление возможности пагинации
     add_pagination(application)
+    application.add_event_handler("startup", make_migrations)
 
     application.add_exception_handler(AuthJWTException, authjwt_exception_handler)
     application.add_exception_handler(RequestValidationError, validation_exception_handler)
